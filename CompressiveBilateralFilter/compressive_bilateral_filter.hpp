67ec5daafb4e79d3476cb59327f27f9b7dd28534
Kenjiro Sugimoto <sugimoto@asagi.waseda.jp>
2015-08-24 10:18:31 +0100

    Add dual-domain error minimization for O(1) Gaussian filter

--- CompressiveBilateralFilter/compressive_bilateral_filter.hpp
+++ CompressiveBilateralFilter/compressive_bilateral_filter.hpp
@@ -16,13 +16,13 @@
 
 //==================================================================================================
 
-// a scale-adjusted derivative of the estimated kernel error E(T)
-class derivative_estimated_kernel_error
+// a scale-adjusted derivative of the estimated Gaussian range kernel error
+class derivative_estimated_gaussian_range_kernel_error
 {
 private:
 	double s,kappa;
 public:
-	derivative_estimated_kernel_error(double s,int K):s(s),kappa(M_PI*(2*K+1)){}
+	derivative_estimated_gaussian_range_kernel_error(double s,int K):s(s),kappa(M_PI*(2*K+1)){}
 public:
 	double operator()(double T)
 	{
@@ -39,19 +39,15 @@ inline double solve_by_bs(Functor df,double x1,double x2,int loop=10)
 	for(int i=0;i<loop;++i)
 	{
 		double x=(x1+x2)/2.0;
-		double dy=df(x);
-		if(0.0<=dy)
-			x2=x;
-		else
-			x1=x;
+		((0.0<=df(x))?x2:x1)=x;
 	}
 	return (x1+x2)/2.0;
 }
 
 // s has to be normalized as R=1
-inline std::pair<int,double> optimize_K_and_T(double s,double tau)
+inline std::pair<int,double> estimate_optimal_K_and_T(double s,double tau)
 {
-	const double xi=boost::math::erfc_inv(tau*tau);
+	double xi=boost::math::erfc_inv(tau*tau);
 	int K=static_cast<int>(std::ceil(xi*xi/(2.0*M_PI)+xi/(2.0*M_PI*s)-0.5));
 
 	// It is better to slightly extend the original search domain D
@@ -59,7 +55,8 @@ inline std::pair<int,double> optimize_K_and_T(double s,double tau)
 	const double magicnum=0.03;
 	double t1=s*xi+1.0;
 	double t2=M_PI*s*(2*K+1)/xi+magicnum;
-	double T=solve_by_bs(derivative_estimated_kernel_error(s,K),t1,t2);
+	derivative_estimated_gaussian_range_kernel_error df(s,K);
+	double T=solve_by_bs(df,t1,t2);
 	return std::make_pair(K,T);
 }
 
@@ -68,7 +65,7 @@ template<int TONE>
 void apply_bilateral_filter_compressive(const cv::Mat_<double>& src,cv::Mat_<double>& dst,double ss,double sr,double tau)
 {
 	// computing parameters
-	std::pair<int,double> params=optimize_K_and_T(sr,tau);
+	std::pair<int,double> params=estimate_optimal_K_and_T(sr,tau);
 	int K=params.first;
 	double T=params.second;
 	
