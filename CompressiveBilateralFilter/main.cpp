bfccbe604635fa8a39b4389968781c8145aa0bc1
Kenjiro Sugimoto <sugimoto@asagi.waseda.jp>
2015-09-04 17:05:39 +0100

    Add cross/joint bilateral filter

--- CompressiveBilateralFilter/main.cpp
+++ CompressiveBilateralFilter/main.cpp
@@ -29,14 +29,11 @@
 #pragma comment(lib, "opencv_contrib"CV_VERSION_NUMBER".lib")
 #endif
 
-// parameters of BF algorithms (assuming 8-bits dynamic range)
+// assuming 8-bits dynamic range
 const double tone=256.0;
-const double sigmaS=2.0;
-const double sigmaR=0.1*(tone-1.0);
-const double tol=0.1; // for compressive BF
 
 const bool sw_imshow=true;
-//const bool sw_imwrite=false;
+const bool sw_imwrite=false;
 
 double calc_psnr(const cv::Mat& image1,const cv::Mat& image2,double maxval)
 {
@@ -62,30 +59,40 @@ double calc_psnr(const cv::Mat& image1,const cv::Mat& image2,double maxval)
 	return snr;
 }
 
-int main(int argc,char** argv)
+template<typename T>
+void clip(cv::Mat& image,double minval=0.0,double maxval=255.0)
 {
-	if(argc!=2)
+	for(int y=0;y<image.rows;++y)
 	{
-		std::cerr<<"Usage: cbf [ImageInputPath]"<<std::endl;
-		return 1;
+		T* p=image.ptr<T>(y);
+		for(int x=0;x<image.cols;++x)
+		{
+			for(int c=0;c<image.channels();++c)
+			{
+				const T value=*p;
+				*p++=(value<minval)?minval:(maxval<value)?maxval:value;
+			}
+		}
 	}
+}
 
-	const std::string& pathI(argv[1]);
-	//const std::string& pathI("../lenna-gray.png"); // for debug
-	cv::Mat image0=cv::imread(pathI,-1);
-	if(image0.empty())
+int test_bilateral_filter(const std::string& pathS,double sigmaS,double sigmaR,double tol)
+{
+	cv::Mat imageS=cv::imread(pathS,-1);
+	if(imageS.empty())
 	{
-		std::cerr<<"Input image loading failed!"<<std::endl;
+		std::cerr<<"Source image loading failed!"<<std::endl;
 		return 1;
 	}
 	
-	std::cerr<<"[Input Image]"<<std::endl;
-	std::cerr<<cv::format("\"%s\"  # (w,h,ch)=(%d,%d,%d)",pathI.c_str(),image0.cols,image0.rows,image0.channels())<<std::endl;
+	std::cerr<<"[Loaded Image]"<<std::endl;
+	std::cerr<<cv::format("Source: \"%s\"  # (w,h,ch)=(%d,%d,%d)",pathS.c_str(),imageS.cols,imageS.rows,imageS.channels())<<std::endl;
 	std::cerr<<"[Filter Parameters]"<<std::endl;
-	std::cerr<<cv::format("sigmaS=%f  sigmaR=%f  tol=%f",sigmaS,sigmaR,tol)<<std::endl;
+	std::cerr<<cv::format("sigmaS=%f  sigmaR=%f  tolerance=%f",sigmaS,sigmaR,tol)<<std::endl;
 	
 	cv::Mat src;
-	image0.convertTo(src,CV_64F);
+	imageS.convertTo(src,CV_64F);
+
 	std::vector<cv::Mat_<double> > srcsp;
 	cv::split(src,srcsp);
 
@@ -114,41 +121,144 @@ int main(int argc,char** argv)
 	std::cerr<<cv::format("Compressive BF:  %7.1f [ms]",tm.getTimeMilli())<<std::endl;
 	tm.reset();
 	
-	//// clipping for debug
-	//double minval=  0.0;
-	//double maxval=255.0;
-	//for(int c=0;c<int(src.channels());++c)
-	//{
-	//	for(int y=0;y<src.rows;++y)
-	//	for(int x=0;x<src.cols;++x)
-	//	{
-	//		double p0=dstsp0[c](y,x);
-	//		double p1=dstsp1[c](y,x);
-	//		dstsp0[c](y,x)=(p0<minval)?minval:(maxval<p0)?maxval:p0;
-	//		dstsp1[c](y,x)=(p1<minval)?minval:(maxval<p1)?maxval:p1;
-	//	}
-	//}
-
 	cv::Mat dst0,dst1;
 	cv::merge(dstsp0,dst0);
 	cv::merge(dstsp1,dst1);
+//	clip(dst0); // for debug
+//	clip(dst1); // for debug
 
-	double snr=calc_psnr(dst0,dst1,tone-1.0);
-	std::cerr<<cv::format("SNR:  %f",snr)<<std::endl;
+	double psnr=calc_psnr(dst0,dst1,tone-1.0);
+	std::cerr<<cv::format("PSNR:  %f",psnr)<<std::endl;
 	
 	if(sw_imshow)
 	{
-		//cv::imshow("src",image);
+		//cv::imshow("src",src);
 		cv::imshow("dst0",dst0/(tone-1.0));
 		cv::imshow("dst1",dst1/(tone-1.0));
 		//cv::imshow("error",(dst1-dst0)/(tone-1.0)+0.5);
 		cv::waitKey();
 	}
-	//if(sw_imwrite)
-	//{
-	//	cv::imwrite("../dst0.png",dst0*(tone-1.0));
-	//	cv::imwrite("../dst1.png",dst1*(tone-1.0));
-	//	cv::imwrite("../error.png",(dst1-dst0)+tone/2.0);
-	//}
+	if(sw_imwrite)
+	{
+		//cv::imwrite("../dst0.png",dst0*(tone-1.0));
+		//cv::imwrite("../dst1.png",dst1*(tone-1.0));
+		//cv::imwrite("../error.png",(dst1-dst0)+tone/2.0);
+	}
+	return 0;
+}
+
+int test_crossjoint_bilateral_filter(const std::string& pathS,const std::string& pathG,double sigmaS,double sigmaR,double tol)
+{
+	cv::Mat imageS=cv::imread(pathS,-1);
+	if(imageS.empty())
+	{
+		std::cerr<<"Source image loading failed!"<<std::endl;
+		return 1;
+	}
+	cv::Mat imageG=cv::imread(pathG,-1);
+	if(imageG.empty())
+	{
+		std::cerr<<"Guide image loading failed!"<<std::endl;
+		return 1;
+	}
+
+	std::cerr<<"[Loaded Image]"<<std::endl;
+	std::cerr<<cv::format("Source: \"%s\"  # (w,h,ch)=(%d,%d,%d)",pathS.c_str(),imageS.cols,imageS.rows,imageS.channels())<<std::endl;
+	std::cerr<<cv::format("Guide:  \"%s\"  # (w,h,ch)=(%d,%d,%d)",pathG.c_str(),imageG.cols,imageG.rows,imageG.channels())<<std::endl;
+	if(imageS.size()!=imageG.size() || imageS.channels()!=imageG.channels())
+	{
+		std::cerr<<"Source and guide images should share the same size and channels!"<<std::endl;
+		return 1;
+	}
+
+	std::cerr<<"[Filter Parameters]"<<std::endl;
+	std::cerr<<cv::format("sigmaS=%f  sigmaR=%f  tolerance=%f",sigmaS,sigmaR,tol)<<std::endl;
+	
+	cv::Mat src;
+	imageS.convertTo(src,CV_64F);
+	cv::Mat guide;
+	imageG.convertTo(guide,CV_64F);
+	
+	std::vector<cv::Mat_<double> > srcsp;
+	cv::split(src,srcsp);
+	std::vector<cv::Mat_<double> > guidsp;
+	cv::split(guide,guidsp);
+
+	std::vector<cv::Mat_<double> > dstsp0(src.channels());
+	std::vector<cv::Mat_<double> > dstsp1(src.channels());
+	for(int c=0;c<int(src.channels());++c)
+	{
+		dstsp0[c]=cv::Mat_<double>(src.size());
+		dstsp1[c]=cv::Mat_<double>(src.size());
+	}
+
+	cv::TickMeter tm;
+	// Original cross/joint bilateral filtering
+	tm.start();
+	for(int c=0;c<int(src.channels());++c)
+		apply_crossjoint_bilateral_filter_original(srcsp[c],guidsp[c],dstsp0[c],sigmaS,sigmaR);
+		//apply_bilateral_filter_original(srcsp[c],dstsp0[c],sigmaS,sigmaR);
+	tm.stop();
+	std::cerr<<cv::format("Original BF:     %7.1f [ms]",tm.getTimeMilli())<<std::endl;
+	tm.reset();
+	// Compressive cross/joint bilateral filtering
+	tm.start();
+	compressive_bilateral_filter cbf(sigmaS,sigmaR,tol);
+	for(int c=0;c<int(src.channels());++c)
+		cbf(srcsp[c],dstsp1[c]);
+		//cbf(srcsp[c],guidsp[c],dstsp1[c]);
+	tm.stop();
+	std::cerr<<cv::format("Compressive BF:  %7.1f [ms]",tm.getTimeMilli())<<std::endl;
+	tm.reset();
+	
+	cv::Mat dst0,dst1;
+	cv::merge(dstsp0,dst0);
+	cv::merge(dstsp1,dst1);
+//	clip(dst0); // for debug
+//	clip(dst1); // for debug
+
+	double psnr=calc_psnr(dst0,dst1,tone-1.0);
+	std::cerr<<cv::format("PSNR:  %f",psnr)<<std::endl;
+
+	if(sw_imshow)
+	{
+		//cv::imshow("src",src);
+		//cv::imshow("guide",guide);
+		cv::imshow("dst0",dst0/(tone-1.0));
+		cv::imshow("dst1",dst1/(tone-1.0));
+		//cv::imshow("error",(dst1-dst0)/(tone-1.0)+0.5);
+		cv::waitKey();
+	}
+	if(sw_imwrite)
+	{
+		//cv::imwrite("../dst0.png",dst0*(tone-1.0));
+		//cv::imwrite("../dst1.png",dst1*(tone-1.0));
+		//cv::imwrite("../error.png",(dst1-dst0)+tone/2.0);
+	}
+	return 0;
+}
+
+int main(int argc,char** argv)
+{
+	// parameters of BF algorithms
+	const double sigmaS=2.0;
+	const double sigmaR=0.1*(tone-1.0);
+	const double tol=0.1; // for compressive BF
+	
+	if(argc==2)
+	{
+		//test_bilateral_filter("../lenna-gray.png"); // for debug
+		test_bilateral_filter(argv[1],sigmaS,sigmaR,tol);
+	}
+	else if(argc==3)
+	{
+		//test_crossjoint_bilateral_filter("../flash0s.png","../flash1s.png",sigmaS,sigmaR,tol); // for debug
+		test_crossjoint_bilateral_filter(argv[1],argv[2],sigmaS,sigmaR,tol);
+	}
+	else
+	{
+		std::cerr<<"Usage: cbf [InputImagePath] (GuideImagePath)"<<std::endl;
+		return 1;
+	}
 	return 0;
 }
--- CompressiveBilateralFilter/original_bilateral_filter.hpp
+++ CompressiveBilateralFilter/original_bilateral_filter.hpp
@@ -17,6 +17,78 @@
 #define atE(x) (w-1-std::abs(w-1-(x)))
 #define atS(y) (h-1-std::abs(h-1-(y)))
 
+void apply_crossjoint_bilateral_filter_original(const cv::Mat_<double>& src,const cv::Mat_<double>& guide,cv::Mat_<double>& dst,double sigmaS,double sigmaR,int tone=256)
+{
+	assert(src.size()==guide.size());
+	assert(src.size()==dst.size());
+	
+	const int w=src.cols;
+	const int h=src.rows;
+
+	// generating spatial kernel
+	int r=int(ceil(4.0*sigmaS));
+	cv::Mat_<double> kernelS(1+r,1+r);
+	for(int v=0;v<=r;++v)
+	for(int u=0;u<=r;++u)
+		kernelS(v,u)=exp(-0.5*(u*u+v*v)/(sigmaS*sigmaS));
+	
+	// generating range kernel (discretized for fast computation)
+	std::vector<double> kernelR(tone);
+	for(int t=0;t<tone;++t)
+		kernelR[t]=exp(-0.5*t*t/(sigmaR*sigmaR));
+	
+	// filtering
+	for(int y=0;y<h;++y)
+	for(int x=0;x<w;++x)
+	{
+		int q=int(guide(y,x));
+		double p=src(y,x);
+
+		double numer=1.0, denom=p; // (0,0)
+		for(int u=1;u<=r;++u) // (u,0)
+		{
+			int q0=int(guide(y,atW(x-u)));
+			int q1=int(guide(y,atE(x+u)));
+			double wr0=kernelR[abs(q0-q)]; // guide-based range kernel
+			double wr1=kernelR[abs(q1-q)]; // guide-based range kernel
+			double p0=src(y,atW(x-u));
+			double p1=src(y,atE(x+u));
+			numer+=kernelS(0,u)*(wr0   +wr1   );
+			denom+=kernelS(0,u)*(wr0*p0+wr1*p1);
+		}
+		for(int v=1;v<=r;++v) // (0,v)
+		{
+			int q0=int(guide(atN(y-v),x));
+			int q1=int(guide(atS(y+v),x));
+			double wr0=kernelR[abs(q0-q)]; // guide-based range kernel
+			double wr1=kernelR[abs(q1-q)]; // guide-based range kernel
+			double p0=src(atN(y-v),x);
+			double p1=src(atS(y+v),x);
+			numer+=kernelS(v,0)*(wr0   +wr1   );
+			denom+=kernelS(v,0)*(wr0*p0+wr1*p1);
+		}
+		for(int v=1;v<=r;++v)
+		for(int u=1;u<=r;++u)
+		{
+			int q00=int(guide(atN(y-v),atW(x-u)));
+			int q01=int(guide(atS(y+v),atW(x-u)));
+			int q10=int(guide(atN(y-v),atE(x+u)));
+			int q11=int(guide(atS(y+v),atE(x+u)));
+			double wr00=kernelR[abs(q00-q)]; // guide-based range kernel
+			double wr01=kernelR[abs(q01-q)]; // guide-based range kernel
+			double wr10=kernelR[abs(q10-q)]; // guide-based range kernel
+			double wr11=kernelR[abs(q11-q)]; // guide-based range kernel
+			double p00=src(atN(y-v),atW(x-u));
+			double p01=src(atS(y+v),atW(x-u));
+			double p10=src(atN(y-v),atE(x+u));
+			double p11=src(atS(y+v),atE(x+u));
+			numer+=kernelS(v,u)*(wr00    +wr01    +wr10    +wr11    );
+			denom+=kernelS(v,u)*(wr00*p00+wr01*p01+wr10*p10+wr11*p11);
+		}
+		dst(y,x)=denom/numer;
+	}
+}
+
 void apply_bilateral_filter_original(const cv::Mat_<double>& src,cv::Mat_<double>& dst,double sigmaS,double sigmaR,int tone=256)
 {
 	assert(src.size()==dst.size());
